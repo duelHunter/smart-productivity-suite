595f49ccbaaf75b61734908974586028181bd14e
Dilukshan <dilukshan2020da@gmail.com>
2025-02-23 21:26:31 +0530

    change something

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 const Hero = () => {
   return (
     <section className="flex flex-col items-center text-center py-16 px-4">
-      <h1 className="text-5xl font-bold mt-20 mb-6 text-black">Digital Tools</h1>
+      <h1 className="text-5xl font-bold mt-12 lg:mt-20 mb-6 text-black">Digital Tools</h1>
       <p className="text-gray-500 mt-2">
         Access our comprehensive collection of digital tools designed to enhance your productivity
       </p>
